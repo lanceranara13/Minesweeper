ca2e57a9fded42e54a37e708ab37777d5e7d91f0
Lance <ranaralance13@gmail.com>
2019-12-31 00:44:16 +0800

    12/31/19 update

--- minesweeper.cpp
+++ minesweeper.cpp
@@ -10,12 +10,14 @@
 //  4/22/19 - Creates a solved matrix
 //  4/23/19 - User can now input the coordinate of the matrix and get an answer
 //          - Created a new blank board that will serve as play area
+// 12/31/19 - Replaced string array to vector string
 //
 
 
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef vector<vector<string>> vvs;
 #define SIZE 10
 #define BOMB 30
 
@@ -23,15 +25,26 @@ using namespace std;
 
 vector <int> bombPlacement;
 
+void initialize(vvs &square, int r, int c){
+    for(int i = 0; i < r; i++){
+        vector <string> temp;
+        for(int j = 0; j < c; j++){
+            temp.push_back("");
+        }
+        square.push_back(temp);
+    }
+
+}
+
 vector <int> randomGen(){
     srand(time(NULL));
-    
+
     int Initial = 0, number;
     int doubles = SIZE * SIZE;
-    
+
     for(int i = 0; i < BOMB;){
         number = (rand() % (doubles) + 1);
-        
+
         bool checker = true;
         for(int j = 0; j < bombPlacement.size(); j++){
             if(number == bombPlacement[j]){
@@ -39,7 +52,7 @@ vector <int> randomGen(){
                 number = (rand() % (doubles) + 1);
             }
         }
-        
+
         if(checker){
             Initial = number;
             bombPlacement.push_back(number);
@@ -50,19 +63,19 @@ vector <int> randomGen(){
 }
 
 
-void print(string square[][SIZE]){
+void print(vvs square){
     for(int i = -1; i < SIZE;i++){
         if(i!= -1)
             cout << i+1 << " ";
         for(int j = 0; j < SIZE; j++){
             if(i == -1){
                 if(j == 0){
-                    cout << "  "; 
+                    cout << "  ";
                     continue;
                 }
                 else
-                    cout << j << " ";  
-                    
+                    cout << j << " ";
+
                 if(j == SIZE-1)
                     cout << j+1;
             }
@@ -74,11 +87,11 @@ void print(string square[][SIZE]){
     cout << endl;
 }
 
-void number(string square[][SIZE]){
+void number(vvs &square){
     for(int i = 0; i < SIZE; i++){
         for(int j = 0; j < SIZE; j++){
             int counter = 0;
-            
+
             //CORNERS
             if(i == 0 && j == 0){
                 if(square[i][j+1] == "B"){
@@ -93,7 +106,7 @@ void number(string square[][SIZE]){
             }
             else if(i == 0 && j == SIZE-1){
                 if(square[i][j-1] == "B"){
-                    counter++;    
+                    counter++;
                 }
                 if(square[i+1][j] == "B"){
                     counter++;
@@ -124,7 +137,7 @@ void number(string square[][SIZE]){
                     counter++;
                 }
             }
-            
+
             //SIDES
             else if(i == 0 && j > 0){
                 if(square[i][j-1] == "B"){
@@ -194,7 +207,7 @@ void number(string square[][SIZE]){
                     counter++;
                 }
             }
-            
+
             else if(i > 0 && j > 0 && i < SIZE && j < SIZE){
                 if(square[i-1][j] == "B"){
                     counter++;
@@ -222,19 +235,19 @@ void number(string square[][SIZE]){
                 }
             }
             //cout << counter << endl;
-            
+
             if(square[i][j] != "B"){
-                square[i][j] = char(counter+48); 
+                square[i][j] = char(counter+48);
             }
         }
     }
 }
 
-void reveal(string square[SIZE][SIZE], string newBoard[SIZE][SIZE], int x, int y){
+void reveal(vvs &square, vvs &newBoard, int x, int y){
     newBoard[x][y] = square[x][y];
 }
 
-bool finish(string newBoard[SIZE][SIZE]){
+bool finish(vvs &newBoard){
     int counter = 0;
     for(int i = 0; i < SIZE; i++){
         for(int j = 0; j < SIZE; j++){
@@ -249,14 +262,14 @@ bool finish(string newBoard[SIZE][SIZE]){
         return false;
 }
 
-void inputs(string square[SIZE][SIZE], string newBoard[SIZE][SIZE]){
+void inputs(vvs &square, vvs &newBoard){
     string inputx, inputy;
-    
+
     if(finish(newBoard)){
         cout << "CONGRATULATION" << endl;
         quick_exit(0);
     }
-    
+
     cout << "Enter coordinates (x,y): ";
     while(cin >> inputx >> inputy){
         cout << endl;
@@ -266,7 +279,7 @@ void inputs(string square[SIZE][SIZE], string newBoard[SIZE][SIZE]){
                 cout << "CONGRATULATION" << endl;
                 quick_exit(0);
             }
-            
+
             else if(square[x][y] == "B"){
                 cout << endl << "BOOM" << endl << " GAME OVER " << endl;
                 //abort();
@@ -285,51 +298,52 @@ void inputs(string square[SIZE][SIZE], string newBoard[SIZE][SIZE]){
 
         cout << "Enter coordinates (x,y): ";
     }
-    
+
 }
 
-void lcopy(string a[SIZE][SIZE], string b[SIZE][SIZE] ){
+void lcopy(vvs &a, vvs &b){
     for(int i = 0; i < SIZE; i++){
         for(int j = 0; j < SIZE; j++){
-            b[i][j] = a[i][j];    
-        }    
-    }    
+            b[i][j] = a[i][j];
+        }
+    }
 }
 
-void start(string square[][SIZE]){
+void start(vvs &square){
    for(int i = 0; i < SIZE;i++){
         for(int j = 0; j < SIZE; j++){
             square[i][j] = ".";
         }
    }
-   
-   string newBoard[SIZE][SIZE];
+
+   vvs newBoard;
+   initialize(newBoard,SIZE,SIZE);
    lcopy(square, newBoard);
-   
+
    bombPlacement = randomGen();
    sort(bombPlacement.begin(), bombPlacement.end());
-   
+
 //   for(int i = 0; i < bombPlacement.size(); i++){
 //       cout << bombPlacement[i] << endl;
 //   }
 //   cout << endl;
-   
-   
+
+
    int bomb = 1, bombCounter = 0;
    for(int i = 0; i < SIZE;i++){
         for(int j = 0; j < SIZE; j++,bomb++){
             if(bomb == bombPlacement[bombCounter]){
-                square[i][j] = "B"; 
+                square[i][j] = "B";
                 bombCounter++;
             }
         }
    }
-   
+
    number(square);
-   
+
    //print(square);
    print(newBoard);
-   
+
    inputs(square, newBoard);
 }
 
@@ -339,8 +353,11 @@ void start(string square[][SIZE]){
 
 
 
+
 int main(){
-    string square[SIZE][SIZE];
+    //string square[SIZE][SIZE];
+    vvs square;
+    initialize(square, SIZE, SIZE);
     start(square);
     return 0;
 }
